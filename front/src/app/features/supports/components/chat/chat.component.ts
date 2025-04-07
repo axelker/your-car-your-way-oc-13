1c446b561b2269aa1ba26c6fca39a4e767f5af31
axel <axelker@outlook.fr>
2025-04-07 16:29:41 +0200

    feat: add chat component

--- front/src/app/features/supports/components/chat/chat.component.ts
+++ front/src/app/features/supports/components/chat/chat.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth.service';
+import { UserInfo } from '../../../../core/interfaces/user-info';
+import { SessionService } from '../../../../core/services/session.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgFor],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  @Input({required:true}) receiverId! : number;
+  messages: { sender: string, content: string }[] = []; //Todo: init with chat service (findAllByUserId)
+  newMessage: string = '';
+  userInfo! : UserInfo;
 
+  constructor(private chatService: ChatService,private sessionService: SessionService) {
+    this.userInfo = this.sessionService.getUser()!;
+  }
+
+  ngOnInit(): void {
+    this.chatService.listen(`/topic/support/${this.userInfo.id}`).subscribe((msg: any) => {
+      console.log(msg)
+      this.messages.push(msg);
+    });
+  }
+
+  send(): void {
+    if (!this.newMessage.trim()) return;
+
+    const message = {
+      receiverId: this.receiverId,
+      content: this.newMessage,
+      senderId: this.userInfo.id
+    };
+
+    this.chatService.sendMessage('/app/chat.send', message);
+    this.messages.push({ sender: this.userInfo.email, content: this.newMessage });
+    this.newMessage = '';
+  }
 }
--- front/src/app/features/supports/pages/chat-page/chat-page.component.ts
+++ front/src/app/features/supports/pages/chat-page/chat-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ChatComponent } from '../../components/chat/chat.component';
+import { AvaibleUserListComponent } from '../../components/avaible-user-list/avaible-user-list.component';
+import { UserInfo } from '../../../../core/interfaces/user-info';
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [],
+  imports: [ChatComponent,AvaibleUserListComponent],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss'
 })
 export class ChatPageComponent {
+  userSelectedToChat: UserInfo | null = null;
+
+
+  selectUser(user:UserInfo) {
+    this.userSelectedToChat = user;
+    console.log(this.userSelectedToChat)
+  }
+  closeChat() {
+    this.userSelectedToChat = null;
+  }
 
 }
