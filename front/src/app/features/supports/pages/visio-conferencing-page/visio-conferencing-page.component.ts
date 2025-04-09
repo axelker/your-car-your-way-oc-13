77180b24f45c67df6b49c6a980d5ebd79430117e
axel <axelker@outlook.fr>
2025-04-09 12:35:50 +0200

    feat: provide viso comp inside page component

--- front/src/app/features/supports/pages/visio-conferencing-page/visio-conferencing-page.component.ts
+++ front/src/app/features/supports/pages/visio-conferencing-page/visio-conferencing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { VisioComponent } from '../../components/visio/visio.component';
 
 @Component({
   selector: 'app-visio-conferencing-page',
   standalone: true,
-  imports: [],
+  imports: [VisioComponent],
   templateUrl: './visio-conferencing-page.component.html',
   styleUrl: './visio-conferencing-page.component.scss'
 })
